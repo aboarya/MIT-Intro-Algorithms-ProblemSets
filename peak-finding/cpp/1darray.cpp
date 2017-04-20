ba2d7b64ebe2a83a22b36bf7ec54e4b4de7e079a
Sameh Mohamed <sameh@autojazari.com>
2017-04-20 18:46:22 -0400

    added Makefile and updated file doc

--- peak-finding/cpp/1darray.cpp
+++ peak-finding/cpp/1darray.cpp
@@ -1,3 +1,28 @@
+/**
+*	@file    1darray.cpp
+*	@author  Sam Mohamed
+*	@version 0.1
+*	@date    04/20/2017
+*
+*	@brief Find a peak in a 1-d array.
+*
+*	@section DESCRIPTION
+*
+*	Definition: A peak is any elemnt i such that i-1 < i > i+1
+
+*	Proof: We look at the element n/2, then examine the elements on right and left sides.
+*	If we find that n/2 is greater than both its left and right elements, then we are done.
+*	If it is smaller than either of those two elements, then 
+
+*	1) we choose n/2+1 to n and search for a peak in the left half similar to above.
+*	2) we choose n/2-1 to 1 and search for a peak in the right half similar to above.
+
+*	Complexity: O(log(n))
+
+*	Usage:
+*   $ make oned
+*
+*/
 #include <iostream>
 #include <vector>
 #include <stdexcept>
@@ -28,6 +53,6 @@ int find_a_peak(std::vector<int> array)
 int main()
 {
 	std::vector<int> array = {1,2,3,5,4,6,7,8,9};
-	std::cout << find_a_peak(array) << "\n";
+	std::cout << "the peak is " << find_a_peak(array) << "\n";
 
 }
