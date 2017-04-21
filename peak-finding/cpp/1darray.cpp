5bbc551aa68eb94daa3d8f1ac39ce264d6fc158e
Sameh Mohamed <sameh@autojazari.com>
2017-04-21 01:20:29 -0400

    fixed Makefile tabs vs. spaces and updated <= on peak check

--- peak-finding/cpp/1darray.cpp
+++ peak-finding/cpp/1darray.cpp
@@ -35,16 +35,16 @@ int find_a_peak(std::vector<int> array)
     if (len == 2) { return array[1];}
 
     int half = len / 2;
-    if ((array[half-1] <= array[half]) && (array[half] >= array[half+1])) {
+    if ((array[half-1] < array[half]) && (array[half] > array[half+1])) {
         return array[half];
     }
     
-    if (array[half-1] > array[half]) {
+    if (array[half-1] >= array[half]) {
         std::vector<int> slice(array.begin(), array.begin()+half);
         return find_a_peak(slice);
     }
 
-    if (array[half+1] > array[half]) {
+    if (array[half+1] >= array[half]) {
         std::vector<int> slice(array.begin()+half, array.end());
         return find_a_peak(slice);
     }
@@ -52,7 +52,8 @@ int find_a_peak(std::vector<int> array)
 }
 int main()
 {
-    std::vector<int> array = {1,2,3,5,4,6,7,8,9};
+    // std::vector<int> array = {1,2,3,5,4,6,7,8,9};1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    std::vector<int> array = {1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     std::cout << "the peak is " << find_a_peak(array) << "\n";
 
 }
