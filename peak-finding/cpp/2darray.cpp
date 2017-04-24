2e4c01bd4c9bdd42bd406ee6939dde57c753c8fa
Sameh Mohamed <sameh@autojazari.com>
2017-04-24 09:31:03 -0400

    working on 2d peak finding in c++

--- peak-finding/cpp/2darray.cpp
+++ peak-finding/cpp/2darray.cpp
@@ -25,3 +25,85 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+
+class Matrix
+{
+private:
+    size_t rows, columns;
+    std::vector<int> matrix;
+public:
+    Matrix(size_t numrows, size_t numcols) :
+            rows(numrows), columns(numcols), matrix(rows * columns)
+    {
+    }
+
+    int & operator()(size_t row, size_t column)
+    {
+        return matrix[row * columns + column]; // note 2D coordinates are flattened to 1D
+    }
+
+    int operator()(size_t row, size_t column) const
+    {
+        return matrix[row * columns + column];
+    }
+
+};
+
+int find_max(std::vector<int> col)
+{
+	int max = col[0];
+	int index = 0;
+	for (int i=0; i < col.size(); i++)
+	{
+		if (max >= col[i])
+		{
+			max = col[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+int find_a_peak(Matrix matrix)
+{
+	// int len = matrix[0].size();
+	// int m = len / 2;
+	// std::cout << "m is " << m;
+	// std::vector<int> col(3, 0);
+	// for (int i=0; i < m; i++)
+	// 	col[i] = matrix[i][m];
+	
+	// int n = find_max(col);
+	// std::cout << " n is " << n;
+
+	// if (matrix[0].size() == 2) {return matrix[n][m];}
+
+	// if (matrix[n][m-1] < matrix[n][m] > matrix[n][m+1]){return matrix[n][m];}
+	
+	// if (matrix[n][m-1] >= matrix[n][m])
+	// {
+
+ //   		std::vector<std::vector<int> > submatrix; 
+ //    	for (int i=0; i < matrix.size(); i++) {
+ //        	submatrix.push_back(std::vector<int>(matrix[i].begin(), matrix[i].begin() + m));
+ //    	}
+ //    	return find_a_peak(submatrix);
+	// }
+
+	// if (matrix[n][m+1] >= matrix[n][m])
+	// {
+ //   		std::vector<std::vector<int> > submatrix; 
+ //    	for (int i=0; i < matrix.size(); i++) {
+ //        	submatrix.push_back(std::vector<int>(matrix[i].begin() + m, matrix[i].end()));
+ //    	}
+ //    	return find_a_peak(submatrix);
+	// }
+
+	return 0;
+}
+
+int main()
+{
+    Matrix matrix(3,9);
+    std::cout << "the peak is " << find_a_peak(matrix) << "\n";
+}
